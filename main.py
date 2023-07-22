ef08c8ef64839bc6987e314dd90235a3f5af7a2c
ARealConner <81334211+ARealConner@users.noreply.github.com>
2023-07-23 00:59:41 +0900

    Refactor code for clarity and readability
    This commit refactors various sections of 'main.py' in the 'group 7 network ids' project to improve readability and clarity. Changes include rewriting comments as function docstrings, simplifying conditional statements, modifying excessive print statements, and importing specific functions from modules instead of entire modules. This helps in understanding the codebase better and making future modifications more straightforward.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py
 
-'''
+"""
     author: em, co
     project: group 7 network ids model training
     class: CS-534 WPI
@@ -9,21 +9,17 @@
     user interface file
 
     select what type of model to train or test, get metrics, save models, etc
-'''
+"""
 import time
 import joblib
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.svm import SVC
+import pandas as pd
 
-from Utilities.evaluate import *
-from Utilities.prepare_data import *
-from Utilities.utilities import *
-
-from DT.DT import *
-from SVM.SVM import *
-from Data.DataPrep import *
-from Utilities.KendallCorrelation import *
-from Utilities.PearsonsCorrelation import *
+from DT.DT import DT
+from Data.DataPrep import DataPrep
+from SVM.SVM import SVM
+from Utilities.evaluate import evaluate_model
+from Utilities.prepare_data import load_dataset
+from Utilities.utilities import get_results_location, set_model_location, get_model_location
 
 DATA_ROOT = "Data/datasets/CIDDS/"
 raw_data_path = "Data/datasets/CIDDS/CIDDS-001/"
@@ -37,24 +33,16 @@ parallel = joblib.Parallel(n_jobs=2, prefer="threads")
 
 # ********************************************************
 def k_fold_xy(x, y, idx, size):
-    #
-    #   function to segment the k subsets of the data for
-    #       k-folds training and validation
-    #   Note: this is done in development on training set
-    #
-    #   params:
-    #       x: the prepared data to be partitioned
-    #       y: the prepared labels to be partitioned
-    #       idx: k-val * iteration
-    #       size: size of each k-subset
-    #   returns:
-    #       x_train, y_train, x_test, y_test: divided data/labels
-    #           for train and validate cycle (&evaluate)
-    # *********************************************************
-    x_train = []
-    y_train = []
-    x_test = []
-    y_test = []
+    """
+    Segment the k subsets of the data for k-folds training and validation.
+    Note: this is done in development on training set.
+
+    :param x: The prepared data to be partitioned.
+    :param y: The prepared labels to be partitioned.
+    :param idx: k-val * iteration.
+    :param size: Size of each k-subset.
+    :return: Divided data/labels for train and validate cycle (&evaluate).
+    """
 
     front_x = x.iloc[:idx, :]  # start at 0-test subset idx
     back_x = x.iloc[idx + size:, :]  # start after set
@@ -74,19 +62,17 @@ def k_fold_xy(x, y, idx, size):
 
 # *********************************************************
 def k_fold_train_and_validate(k, model_type, filename, model_obj, data_length=0):
-    #
-    #   function to perform cross validation on dataset
-    #       dataset is divided into k subsets, with k-1 used
-    #       to train, and 1 left to validate on
-    #
-    #   k: number of subdivisions to make from the dataset
-    #   model_type: type of model to work with (from user selection)
-    #   model_name: name of the model
-    #   filename: data file to use for dataset
-    #
-    #   returns:
-    #       nothing, but saves model, saves results to model folder
-    # *********************************************************
+    """
+    Perform cross-validation on dataset. Dataset is divided into k subsets,
+    with k-1 used to train, and 1 left to validate on.
+
+    :param data_length: Length of data to use (0 for all).
+    :param model_obj: Model object.
+    :param k: Number of subdivisions to make from the dataset.
+    :param model_type: Type of model to work with (from user selection).
+    :param filename: Data file to use for dataset.
+    :return: None. Saves model and results to model folder.
+    """
     df = load_dataset(filename)
     x, y = model_obj.prepare_data(df, attributes, data_length)
 
@@ -102,7 +88,7 @@ def k_fold_train_and_validate(k, model_type, filename, model_obj, data_length=0)
         # this is just for debugging and to short circuit on bad data
         # maybe should move this to preparing data?
         value = x_train.isnull().sum().sum()
-        if (value > 0):
+        if value > 0:
             print("null x vals: " + str(value))
             for col in attributes:
                 num = x_train.isnull().sum()
@@ -121,33 +107,30 @@ def k_fold_train_and_validate(k, model_type, filename, model_obj, data_length=0)
 
 # ***********************************************************
 def render(model_obj, trained_model, x_train, y_train):
-    #
-    #   ask before rendering
-    #
-    # ***********************************************************
-    # this is sticky, press enter after closing the render (in my experience)
-    render = input("render model? (y/n): ")
-    if (render == 'y'):
+    """
+    Ask before rendering.
+
+    :param model_obj: Model object.
+    :param trained_model: Trained model.
+    :param x_train: Training data.
+    :param y_train: Training labels.
+    """
+    render_model = input("render model? (y/n): ")
+    if render_model == 'y':
         model_obj.render_model(trained_model, x_train, y_train)
 
 
 # **********************************************************
 def train(x, y, model, model_type):
-    #
-    #   Train the model on prepared dataset
-    #
-    #   params:
-    #       x: x data (training)
-    #       y: y data (training - labels)
-    #       model: a model object
-    #       model_type: the type of model (where to save model)
-    #   calls:
-    #       model.train_model(x, y)
-    #       get_results_location: save training time
-    #       set_model_location: save the trained model
-    #   returns:
-    #       trained_model: the trained model
-    # ***********************************************************
+    """
+    Train the model on prepared dataset.
+
+    :param x: Training data.
+    :param y: Training labels.
+    :param model: Model object.
+    :param model_type: Type of model (where to save model).
+    :return: Trained model.
+    """
     print("Training model...")
 
     start_time = time.time()
@@ -167,21 +150,15 @@ def train(x, y, model, model_type):
 
 # **************************************************************
 def test(x_test, y_test, model_type, model_obj):
-    #
-    #   function to test the model after training
-    #
-    #   params:
-    #       x_test: test data (samples)
-    #       y_test: test data labels
-    #       model_type: type of model being trained
-    #       model_obj: model object
-    #   calls:
-    #       load_saved_model():loads trained model
-    #       model_obj.test_model(): tests the model (predict)
-    #       evaluate_model(): saves performance results to file
-    #   returns:
-    #       nothing, but saves data from evaluation
-    # **************************************************************
+    """
+    Test the model after training.
+
+    :param x_test: Test data.
+    :param y_test: Test labels.
+    :param model_type: Type of model being trained.
+    :param model_obj: Model object.
+    :return: None. Saves evaluation data.
+    """
     trained_model = load_saved_model(model_type, model_obj.model_name)
     model_obj.set_model(trained_model)
     predictions = model_obj.test_model(x_test)
@@ -190,17 +167,16 @@ def test(x_test, y_test, model_type, model_obj):
 
 # ***********************************************************
 def train_or_test():
-    #
-    #   select to train, k-folds, or test with an existing model
-    #
-    #   returns:
-    #       choice (int): 0 for train, 1 for k-folds, 2 for test
-    # ************************************************************
+    """
+    Select to train, k-folds, or test with an existing model.
+
+    :return: Choice (int): 0 for train, 1 for k-folds, 2 for test.
+    """
     while True:
         for i in range(len(opts)):
             print(str(i) + ': ' + opts[i])
         choice = input("Select an activity: ")
-        if (int(choice) in [0, 1, 2]):
+        if int(choice) in [0, 1, 2]:
             break
         else:
             print("Invalid entry, please try again")
@@ -209,53 +185,42 @@ def train_or_test():
 
 # ****************************************************************
 def load_saved_model(model_type, model_name):
-    #
-    #   load a saved model, if it exists
-    #
-    #   params:
-    #       model_type: type of model
-    #       model_name: name of model
-    #   returns:
-    #       model: saved model, or none if error
-    # ***************************************************************
-    model = None
+    """
+    Load a saved model, if it exists.
+
+    :param model_type: Type of model.
+    :param model_name: Name of model.
+    :return: Saved model or None if error.
+    """
     try:
         model = joblib.load(get_model_location(model_type, model_name))
         print("Loading model...")
-    except:
+    except FileNotFoundError:
+        model = None
         print("could not load model")
 
     return model
 
 
 # ****************************************************************
-def get_model_name(model_type, opt):
-    #
-    #   get the model name to use
-    #       if using an existing model, verify that the model exists
-    #       let user try again if the model was not found
-    #
-    #   params:
-    #       model_type: type of model to work with
-    #       opt: a number corresponding to the selection
-    #           (0)- train (1)- k-folds (2)- test
-    #   return:
-    #       model_name: name of the model
-    #
-    #   TODO: implement warm start to iteratively train the same model
-    #       (reason to allow loading an existing model with train opt)
-    # *****************************************************************
+def get_model_name(model_type):
+    """
+    get the model name to use
+    if using an existing model, verify that the model exists
+    let user try again if the model was not found
+    :param model_type: type of model to work with
+    :return: name of the model
+    """
     use_existing = input("Use existing model? (y/n): ") == 'y'
     model_name = input("Enter model name (enter for default): ")
 
     while True:
-        if (use_existing):
+        if use_existing:
             # test if it exists
             try:
                 print(get_model_location(model_type, model_name))
-                model_exist = get_model_location(model_type, model_name)
                 break
-            except:
+            except FileNotFoundError:
                 print("model does not exist")
         else:
             break
@@ -278,7 +243,7 @@ def get_model_type():
     while True:
         t = input("Select model type: ")
         idx = int(t)
-        if (idx > len(MODEL_ROOT) or (idx < 0)):
+        if idx > len(MODEL_ROOT) or (idx < 0):
             print("Invalid selection. Please try again")
         else:
             break
@@ -287,33 +252,29 @@ def get_model_type():
 
 # **********************************************************
 def main():
-    #
-    #   main function to control program flow
-    #
-    # ***********************************************************
+    """
+    Main function to control program flow.
+    """
 
-    ## BEGIN CLEANING/NORMALIZATION/TRAIN AND TEST SPLIT OF RAW DATA
+    # BEGIN CLEANING/NORMALIZATION/TRAIN AND TEST SPLIT OF RAW DATA
 
-    ## INSTANTIATE THE DATAPREP CLASS
+    # INSTANTIATE THE DATAPREP CLASS
     data_opt = DataPrep(raw_data_path, DATA_ROOT)
 
-    ## IF USER HAS A RAW CSV TO PARSE
+    # IF USER HAS A RAW CSV TO PARSE
     data_opt.set_raw_dir()
 
     if data_opt.get_raw_dir():
 
-        ## BEGIN PARSING THE DATA
+        # BEGIN PARSING THE DATA
         data_opt.set_parse_data()
 
-        ## BEGIN SPLITTING THE DATA
+        # BEGIN SPLITTING THE DATA
         data_opt.split_data()
 
-    ## IF DATA HAS ALREADY BEEN PARSED
-    else:
-        pass
+    # IF DATA HAS ALREADY BEEN PARSED
 
-    ## BEGIN SELECTION OF MODELS
-    model = None
+    # BEGIN SELECTION OF MODELS
     length = -1  # default to use full dataset in training/testing
 
     opt = train_or_test()
@@ -322,11 +283,11 @@ def main():
     model_type = get_model_type()
 
     # get the model name to work with (may exist, if not model obj will be named)
-    model_name = get_model_name(model_type, opt)
+    model_name = get_model_name(model_type)
 
-    if (model_type == "DT"):
+    if model_type == "DT":
         model = DT(model_name)
-    elif (model_type == "SVM"):
+    elif model_type == "SVM":
         model = SVM(['Dst_Pt', 'Src_IP', 'Bytes', 'Label'], model_name)
         length = 50000  # demo length - SVM training is long
     else:
@@ -334,20 +295,20 @@ def main():
         return
     # TODO: add FKM and TACGAN
 
-    if (opt == 0):  # train
+    if opt == 0:  # train
         df = load_dataset(train_filename)
         x, y = model.prepare_data(df, attributes, length)
         trained_model = train(x, y, model, model_type)
         render(model, trained_model, x, y)
-    elif (opt == 1):  # cross validation routine
+    elif opt == 1:  # cross validation routine
         k_fold_train_and_validate(10, model_type, train_filename, model, length)
     else:  # test
         df = load_dataset(test_filename)
         x, y = model.prepare_data(df, attributes, length)
         try:
-            trained_model = load_saved_model(model_type, model_name)
+            load_saved_model(model_type, model_name)
             test(x, y, model_type, model)
-        except:
+        except FileNotFoundError:
             print("cannot load model for testing")
 
 
